84cc0fc3d6395a5e3af6db3e9e34ff32813a6966
Suraj Kanuganti <115855510+surajvarma1@users.noreply.github.com>
2024-11-11 15:23:15 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,9 +1,26 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        for i in range(len(s)):
+            # If the current value is less than the next value, subtract it
+            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        
+        return total
+
+# Example usage:
+solution = Solution()
+print(solution.romanToInt(input()))     
+
+
         pass
 
 
